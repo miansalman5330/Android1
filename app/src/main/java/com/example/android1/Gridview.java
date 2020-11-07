e603d336769296f0c7a14d9ffc63db9ca759b490
Mian Salman <miansalman5330@gmail.com>
2020-11-07 11:51:38 -0800

    version1

---
+++ app/src/main/java/com/example/android1/Gridview.java
@@ -0,0 +1,14 @@
+package com.example.android1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Gridview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridview);
+    }
+}
